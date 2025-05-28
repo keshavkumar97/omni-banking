ae43433a1d884a6b7f2be6b6bb2bc5183a34b75e
Keshav Kumar <keshav_kumar@epam.com>
2025-05-28 15:10:18 +0530

    duplicate import removed

--- customer-service/src/main/java/org/omni/bank/customer/service/impl/CustomerServiceImpl.java
+++ customer-service/src/main/java/org/omni/bank/customer/service/impl/CustomerServiceImpl.java
@@ -11,7 +11,6 @@ import org.omni.bank.customer.util.CustomerMapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
