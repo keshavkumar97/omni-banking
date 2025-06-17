85b7ca23d48148298f6ef5119332b10b60e24810
Keshav Kumar <keshav_kumar@epam.com>
2025-06-17 22:37:51 +0530

    unused import removed

--- auth-service/src/main/java/org/omni/bank/auth/service/RoleService.java
+++ auth-service/src/main/java/org/omni/bank/auth/service/RoleService.java
@@ -1,6 +1,6 @@
 package org.omni.bank.auth.service;
 
-import org.omni.bank.auth.model.Role;
+
 
 public interface RoleService {
 //    Role createRole(String roleName, String description, String roleCode);
